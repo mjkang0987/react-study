35f25a74349a0c66df3c4d1081288f4356a47adb
minjeongkang <snowdrop1202@gmail.com>
2020-09-29 01:16:20 +0900

    [섹션1] 리액트 프로젝트 시작하기 - 바벨 사용해 보기
    react-200928

---
+++ example/20200928/babel/simple.js
@@ -0,0 +1,28 @@
+const LikeButton = () => {
+  const [liked, setLiked] = React.useState(false);
+  const text = liked ? '좋아요 취소' : '좋아요';
+  return /*#__PURE__*/React.createElement("button", {
+    onClick: () => setLiked(!liked)
+  }, text);
+};
+
+const Container = () => {
+  const [count, setCount] = React.useState(0);
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(LikeButton, null), /*#__PURE__*/React.createElement("div", {
+    style: {
+      marginTop: 20
+    }
+  }, /*#__PURE__*/React.createElement("span", null, "\uC804\uCCB4 \uCE74\uC6B4\uD2B8"), /*#__PURE__*/React.createElement("span", {
+    style: {
+      marginRight: 10
+    }
+  }, count), /*#__PURE__*/React.createElement("button", {
+    onClick: () => setCount(count + 1)
+  }, "\uC99D\uAC00"), /*#__PURE__*/React.createElement("button", {
+    onClick: () => setCount(count - 1)
+  }, "\uAC10\uC18C")));
+};
+
+const ROOT = 'root';
+const domContainer = document.getElementById(ROOT);
+ReactDOM.render(React.createElement(Container), domContainer);
---
+++ example/20200928/babel/simple_.js
@@ -0,0 +1,55 @@
+const LikeButton = () => {
+  const [liked, setLiked] = React.useState(false);
+  const text = liked ? '좋아요 취소' : '좋아요';
+  return React.createElement(
+    'button',
+    {
+      onClick: () => setLiked(!liked)
+    },
+    text
+  );
+}
+
+const Container = () => {
+  const [count, setCount] = React.useState(0);
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(LikeButton),
+    React.createElement(
+      'div',
+      {
+        style: {marginTop: 20}
+      },
+      React.createElement(
+        'span',
+        null,
+        '전체 카운트'
+      ),
+      React.createElement(
+        'span',
+        {
+          style: {marginRight: 10}
+        },
+        count
+      ),
+      React.createElement(
+        'button',
+        {
+          onClick: () => setCount(count + 1)
+        },
+        '증가'
+      ),
+      React.createElement(
+        'button',
+        {
+          onClick: () => setCount(count - 1)
+        },
+        '감소'
+      )
+    )
+  )
+}
+const ROOT = 'root';
+const domContainer = document.getElementById(ROOT);
+ReactDOM.render(React.createElement(Container), domContainer);
