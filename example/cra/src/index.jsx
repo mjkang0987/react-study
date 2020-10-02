d832b474f3e5a65196ef509287e6a0dfe54a0462
minjeongkang <snowdrop1202@gmail.com>
2020-10-02 20:48:17 +0900

    [섹션1] SPA 만들기
    - react-router-dom 사용하지 않고 SPA 만들기
    
    #8-20201002

--- example/cra/src/index.jsx
+++ example/cra/src/index.jsx
@@ -1,15 +1,57 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
-import Box from './Box';
-import Button from './Button';
-import './index.css';
+
+const App = () => {
+  const [pageName, setPageName] = useState('');
+  useEffect(() => {
+    window.onpopstate = event => {
+      setPageName(event.state);
+    }
+  }, []);
+  const handler = event => {
+    const target = event.target;
+    const pageData = target.dataset.page;
+      window.history.pushState(
+        `${pageData}`,
+        '',
+        `/${pageData}`
+      )
+    setPageName(pageData);
+  }
+  return (
+    <div>
+      <button
+        onClick={handler}
+        data-page="page1">
+        page1
+      </button>
+      <button
+        onClick={handler}
+        data-page="page2">
+        page2
+      </button>
+      {!pageName && <Home />}
+      {pageName === 'page1' && <Page1 />}
+      {pageName === 'page2' && <Page2 />}
+    </div>
+  )
+}
+
+const Home = () => {
+  return <h2>여기는 홈페이지 입니다. 원하는 페이지 버튼을 선택하세요.</h2>
+}
+
+const Page1 = () => {
+  return <h2>여기는 Page1 입니다.</h2>
+}
+
+const Page2 = () => {
+  return <h2>여기는 Page2 입니다.</h2>
+}
 
 ReactDOM.render(
-  <div>
-    <Button size="big" />
-    <Button size="small" />
-    <Box size="big" />
-    <Box size="small" />
-  </div>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
