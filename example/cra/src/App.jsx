b11de7e726f99414d0b6d8445b074b65e3f6adb0
minjeongkang <snowdrop1202@gmail.com>
2020-10-02 21:25:43 +0900

    [섹션1] SPA 만들기
    - react-router-dom 사용 예제
    
    #8-20201002

--- example/cra/src/App.jsx
+++ example/cra/src/App.jsx
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import Rooms from './Rooms';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>hello</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div
+        style={{padding: 20, border: '5px solid gray'}}>
+        <p>
+          <Link to='/'>홈</Link>
+        </p>
+        <p>
+          <Link to='/photo'>사진</Link>
+        </p>
+        <p>
+          <Link to='/rooms'>방 소개</Link>
+        </p>
+        <Route
+          exact
+          path='/'
+          component={Home} />
+        <Route
+          path='/photo'
+          component={Photo} />
+        <Route
+          path='/rooms'
+          component={Rooms} />
+      </div>
+    </BrowserRouter>
   );
+}
+
+const Home = () => {
+  return <h2>여기는 홈페이지 입니다. 원하는 페이지 버튼을 선택하세요.</h2>
+}
+
+const Photo = () => {
+  return <h2>여기서 사진을 감상하세요.</h2>
 }
 
 export default App;
--- example/cra/src/index.jsx
+++ example/cra/src/index.jsx
@@ -1,53 +1,6 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () => {
-  const [pageName, setPageName] = useState('');
-  useEffect(() => {
-    window.onpopstate = event => {
-      setPageName(event.state);
-    }
-  }, []);
-  const handler = event => {
-    const target = event.target;
-    const pageData = target.dataset.page;
-      window.history.pushState(
-        `${pageData}`,
-        '',
-        `/${pageData}`
-      )
-    setPageName(pageData);
-  }
-  return (
-    <div>
-      <button
-        onClick={handler}
-        data-page="page1">
-        page1
-      </button>
-      <button
-        onClick={handler}
-        data-page="page2">
-        page2
-      </button>
-      {!pageName && <Home />}
-      {pageName === 'page1' && <Page1 />}
-      {pageName === 'page2' && <Page2 />}
-    </div>
-  )
-}
-
-const Home = () => {
-  return <h2>여기는 홈페이지 입니다. 원하는 페이지 버튼을 선택하세요.</h2>
-}
-
-const Page1 = () => {
-  return <h2>여기는 Page1 입니다.</h2>
-}
-
-const Page2 = () => {
-  return <h2>여기는 Page2 입니다.</h2>
-}
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
