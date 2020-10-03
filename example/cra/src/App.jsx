38c2ccbe5ddf065bf091c2a1ed8254938f3186ca
minjeongkang <snowdrop1202@gmail.com>
2020-10-03 18:33:13 +0900

    [섹션2] 리액트를 사용한 코드의 특징
    - 이전 작업 파일, 소스 삭제
    
    #10-20201003

--- example/cra/src/App.jsx
+++ example/cra/src/App.jsx
@@ -1,42 +1,11 @@
-import React from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
-import Rooms from './Rooms';
+import React, {useState} from 'react';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div
-        style={{padding: 20, border: '5px solid gray'}}>
-        <p>
-          <Link to='/'>홈</Link>
-        </p>
-        <p>
-          <Link to='/photo'>사진</Link>
-        </p>
-        <p>
-          <Link to='/rooms'>방 소개</Link>
-        </p>
-        <Route
-          exact
-          path='/'
-          component={Home} />
-        <Route
-          path='/photo'
-          component={Photo} />
-        <Route
-          path='/rooms'
-          component={Rooms} />
-      </div>
-    </BrowserRouter>
+    <div>
+
+    </div>
   );
-}
-
-const Home = () => {
-  return <h2>여기는 홈페이지 입니다. 원하는 페이지 버튼을 선택하세요.</h2>
-}
-
-const Photo = () => {
-  return <h2>여기서 사진을 감상하세요.</h2>
 }
 
 export default App;
