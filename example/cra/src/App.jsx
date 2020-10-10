ad887b7869a880eeed7e57a332164418ec65fd60
minjeongkang <snowdrop1202@gmail.com>
2020-10-10 23:46:55 +0900

    [섹션2] 리액트 훅 기초 익히기2
    - 예제 추가
    
    #14-20201010

--- example/cra/src/App.jsx
+++ example/cra/src/App.jsx
@@ -1,25 +1,17 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
+import WidthPrinter from "./WidthPrinter";
 
 const App = () => {
-  const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(0);
-  const onClick = () => {
-    setCount1(count1+1);
-    setCount2(count2+1);
-  }
-
-  console.log('render Called');
-  useEffect(() => {
-    document.title = `업데이트 횟수: ${count1}`;
-  });
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h2>{count1}</h2>
-      <h2>{count2}</h2>
-      <button onClick={onClick}>증가</button>
-    </>
-  );
+    <div>
+      {count === 0 &&
+        <WidthPrinter/>
+      }
+      <button onClick={() => setCount(count + 1)}>증가</button>
+    </div>
+  )
 }
 
 export default App;
