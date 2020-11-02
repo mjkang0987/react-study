df420db4b47aaaf1197e7b1a00e6c99f2932b5ee
minjeongkang <snowdrop1202@gmail.com>
2020-11-03 01:46:54 +0900

    [섹션4] 리액트 내장 훅
    - index.jsx 롤백
    
    #20-20201102

---
+++ example/cra/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import UseRef from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <UseRef />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
