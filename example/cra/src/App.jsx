2134990e32faea12213ccabdf667faf179702de2
minjeongkang <snowdrop1202@gmail.com>
2020-11-04 00:40:31 +0900

    [섹션4] 리액트 내장 훅
    - 예제 추가
    
    #20-20201102

--- example/cra/src/App.jsx
+++ example/cra/src/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import UseImperativeHandleParent from "./UseImperativeHandleParent";
+import UseDebugValue, {
+  STATE_START,
+  STATE_RUNNING
+} from './UseDebugValue';
 
 
-const UseRef = () => {
+const App = () => {
+  const [state, next] = UseDebugValue(true);
+  const msg =
+    state === STATE_START
+      ? '앱 시작'
+      : state === STATE_RUNNING
+      ? '앱 시작'
+      : '앱 종료';
   return (
     <div>
-      <UseImperativeHandleParent/>
+      <p>{msg}</p>
+      <button onClick={next}>next</button>
     </div>
   );
 }
 
-export default UseRef;
+export default App;
--- example/cra/src/index.jsx
+++ example/cra/src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import UseRef from './App';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <UseRef />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
