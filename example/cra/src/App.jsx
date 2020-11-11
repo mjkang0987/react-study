bb73e5c349469c18af414c14ca596ca7f407b75f
minjeongkang <snowdrop1202@gmail.com>
2020-11-11 22:55:53 +0900

    [섹션5] 랜더링 속도를 올리기 위한 성능 최적화 방법
    - 기존 예제 삭제
    
    #26-20201111

--- example/cra/src/App.jsx
+++ example/cra/src/App.jsx
@@ -1,39 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import useOnMounted from './useOnMounted';
+import React from 'react';
 
 const App = () => {
   return (
     <div>
-      <MyComponent/>
+      react
     </div>
   )
 }
-
-const MyComponent = () => {
-  const [value1, setValue1] = useState(0);
-  const [value2, setValue2] = useState(0);
-  useEffect(() => {
-    const id = setInterval(() => console.log(value1, value2), 1000);
-    return () => clearInterval(id);
-  }, [value1])
-  return (
-    <div>
-      <button onClick={() => setValue1(value1 + 1)}>value1 증가</button>
-      <button onClick={() => setValue2(value2 + 1)}>value2 증가</button>
-    </div>
-  )
-}
-
-const Profile = ({userId}) => {
-  const [user, setUser] = useState();
-  const [needDetail, setNeedDetail] = useState(false);
-
-  useOnMounted(() => fetchUser(userId));
-
-  console.log(user, setNeedDetail);
-  return null;
-}
-
-const fetchUser = () => {}
 
 export default App;
