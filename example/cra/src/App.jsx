c6f3e3e2c79eac2bf9608cd3cdbebd8124d1711e
minjeongkang <snowdrop1202@gmail.com>
2020-11-14 00:05:51 +0900

    [섹션5] 랜더링 속도를 올리기 위한 성능 최적화 방법
    - 세 번째 영상 예제 추가
    
    #26-20201113

--- example/cra/src/App.jsx
+++ example/cra/src/App.jsx
@@ -1,34 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Fruits from './Fruits';
+import Flag from './Flag';
+import Change from './Change';
+import Array from './Array';
 
 const App = () => {
-  const [fruits, setFruits] = useState(['apple', 'banana', 'orange']);
-  const [newFruits, setNewFruits] = useState('');
-  const addNewFruit = _ => {
-    setFruits([...fruits, newFruits])
-    // fruits.push(newFruits);
-    setNewFruits('');
-  }
   return (
     <div>
-     <Selected options={fruits} />
-     <input
-       type="text"
-       value={newFruits}
-       onChange={e => setNewFruits(e.target.value)}
-     />
-     <button
-       onClick={addNewFruit}
-     >추가하기</button>
+      <Array/>
     </div>
-  )
-}
-
-const Selected = React.memo(({options}) => (
-  <div>
-    {options.map((items, index) => (
-      <p key={`${items}${index}`}>{items}</p>
-    ))}
-  </div>
-));
+  );
+};
 
 export default App;
